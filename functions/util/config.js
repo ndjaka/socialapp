57191ad8adab83054d99275ceca12c51b732070b
Ndjaka <eugenendjaka@gmail.com>
2019-12-27 16:55:41 +0100

    socialapp

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  apiKey: 'AIzaSyBGE61bBXSyRfOccvujvR8VjWfB1BjW-Og',
-  authDomain: 'socialape-d081e.firebaseapp.com',
-  databaseURL: 'https://socialape-d081e.firebaseio.com',
-  projectId: 'socialape-d081e',
-  storageBucket: 'socialape-d081e.appspot.com',
-  messagingSenderId: '354040522108'
+  apiKey: "AIzaSyC8jpwxhLIUFXSx96jHCkO7JK1GNbl7Ro8",
+  authDomain: "socialapp-36fed.firebaseapp.com",
+  databaseURL: "https://socialapp-36fed.firebaseio.com",
+  projectId: "socialapp-36fed",
+  storageBucket: "socialapp-36fed.appspot.com",
+  messagingSenderId: "1046587970825",
+  appId: "1:1046587970825:web:65e586e4521879ccb56fd8",
+  measurementId: "G-N3HV3MELQ0"
 };
+
